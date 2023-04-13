637bc64b4180a393a867896820e0cdd10f533d53
Rodrick C <Rodrickmc42@gmail.com>
2023-04-13 16:16:21 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,142 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [themeOne, setThemeOne] = useState('themeOn');
+  const [themeTwo, setThemeTwo] = useState('themeOff');
+  const [themeThree, setThemeThree] = useState('themeOff');
+  const [pageTheme, setPageTheme] = useState('ThemeOne App');
+  const [equation, setEquation] = useState('00');
+  const [resetFlag, setResetFlag] = useState(false);
+
+
+  function handleEquation(target) {
+    if (equation === '00' || resetFlag) {
+      setEquation(old => '');
+      setResetFlag(old => false);
+    }
+
+    if (target === 'DEL') {
+      let arr = equation.split('');
+      let temp = arr.slice(0, arr.length - 1);
+      temp = temp.join('');
+      if (temp !== '' && equation !== '00') {
+        if ((/^[0-9.+\-*/]+$/).test(temp))
+          setEquation(old => temp)
+      } else (
+        setEquation(old => '00')
+      )
+    } else {
+      if ((/^[0-9.+\-*/]+$/).test(target)) {
+        setEquation(old => old += target)
+      }
+    }
+  }
+
+
+  function handleEvaluation(equation) {
+    //Safer indirect eval call in strict mode
+    //Regex check that only allowed character are in input string
+    //Regex check that the math equation is valid. used chatGPT for this.
+    if ((/^(\d+(\.\d+)?[+\-*\/])+(\d+(\.\d+)?)$/).test(equation)) {
+      let answer = 0;
+      answer = eval?.(`"use strict";(${equation})`);
+      setResetFlag(old => true);
+      setEquation(old => answer);
+    }
+  }
+
+
+  function handleTheme(target) {
+    console.log(target === 'one');
+    switch (target) {
+      case 'one':
+        setThemeOne(old => 'themeOn');
+        setThemeTwo(old => 'themeOff');
+        setThemeThree(old => 'themeOff');
+        setPageTheme(old => 'ThemeOne App');
+        break;
+      case 'two':
+        setThemeOne(old => 'themeOff');
+        setThemeTwo(old => 'themeOn');
+        setThemeThree(old => 'themeOff');
+        setPageTheme(old => 'ThemeTwo App');
+        break;
+      case 'three':
+        setThemeOne(old => 'themeOff');
+        setThemeTwo(old => 'themeOff');
+        setThemeThree(old => 'themeOn');
+        setPageTheme(old => 'ThemeThree App');
+        break;
+
+      default:
+        console.log('no match')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={pageTheme}>
+      <section className='hero'>
+        <h1>calc</h1>
+        <div className='themeContainer'>
+          <p>THEME</p>
+          <div className='trippleStepSwitch'>
+            <div className='labelContainer'>
+              <label htmlFor='one'>1</label>
+              <label htmlFor='two'>2</label>
+              <label htmlFor='three'>3</label>
+            </div>
+            <div className='inputContainer'>
+              <input id="one" name='button' type='radio'
+                defaultChecked
+                className={themeOne}
+                onClick={() => handleTheme('one')} />
+              <input id="two" name='button' type='radio'
+                className={themeTwo}
+                onClick={() => handleTheme('two')} />
+              <input id="three" name='button' type='radio'
+                className={themeThree}
+                onClick={() => handleTheme('three')} />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className='Display'>
+        <h2 id='text'>{equation}</h2>
+      </section>
+
+      <section className='numpad'>
+        <div className='row'>
+          <button id='7' onClick={() => handleEquation(7)}><span className='front'>7</span></button>
+          <button id='8' onClick={() => handleEquation(8)}><span className='front'>8</span></button>
+          <button id='9' onClick={() => handleEquation(9)}><span className='front'>9</span></button>
+          <button id='DEL' className='btn--del' onClick={() => handleEquation('DEL')}><span className='front'>DEL</span></button>
+        </div>
+        <div className='row'>
+          <button id='4' onClick={() => handleEquation(4)}><span className='front'>4</span></button>
+          <button id='5' onClick={() => handleEquation(5)}><span className='front'>5</span></button>
+          <button id='6' onClick={() => handleEquation(6)}><span className='front'>6</span></button>
+          <button id='PLUS' onClick={() => handleEquation('+')}><span className='front'>+</span></button>
+        </div>
+        <div className='row'>
+          <button id='1' onClick={() => handleEquation(1)}><span className='front'>1</span></button>
+          <button id='2' onClick={() => handleEquation(2)}><span className='front'>2</span></button>
+          <button id='3' onClick={() => handleEquation(3)}><span className='front'>3</span></button>
+          <button id='MINUS' onClick={() => handleEquation("-")}><span className='front'>-</span></button>
+        </div>
+        <div className='row'>
+          <button id='PERIOD' onClick={() => handleEquation('.')}><span className='front'>.</span></button>
+          <button id='0' onClick={() => handleEquation(0)}><span className='front'>0</span></button>
+          <button id='DIVID' onClick={() => handleEquation('/')}><span className='front'>/</span></button>
+          <button id='MULTIPLY' onClick={() => handleEquation('*')}><span className='front'>*</span></button>
+        </div>
+        <div className='row'>
+          <button id='RESET' className='btn--reset' onClick={() => setEquation(old => '00')}><span className='front'>RESET</span></button>
+          <button id='EQUAL' className='btn--equal' onClick={() => handleEvaluation(equation)}><span className='front'>=</span></button>
+        </div>
+      </section>
     </div>
   );
 }
